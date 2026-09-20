a22791ae76468cf0d392a972b05fd26563ec922c
ancure-2004 <ankur2004tyagi@gmail.com>
2026-09-20 23:13:04 +0530

    Adding DSA Practice

---
+++ DSA Practice/Array and Hashing/1_Counting_Frequencies.cpp
@@ -0,0 +1,43 @@
+// Brute Force 
+class Solution {
+  public:
+    vector<int> frequencyCount(vector<int>& arr) {
+        
+        int n = arr.size();
+        vector<int> result;
+        
+        for(int i = 1; i <= n; i++){
+            int number = 0;
+            for(int j = 0; j < n; j++){
+                if(arr[j] == i){
+                    number++;
+                }
+            }
+            result.push_back(number);
+        }
+        
+        return result;
+    }
+};
+
+
+//Optimized
+class Solution {
+  public:
+    vector<int> frequencyCount(vector<int>& arr) {
+        
+        int n = arr.size();
+
+        vector<int> hash(n + 1);
+        for(int i = 0; i < n; i++){
+            hash[arr[i]]++;
+        }
+        
+        vector<int> result(n);
+        for(int i = 0; i < n; i++){
+            result[i] = hash[i + 1];
+        }
+        
+        return result;
+    }
+};
